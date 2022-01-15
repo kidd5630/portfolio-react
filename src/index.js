24809ca8576908f1793fbe2771edeb728c9afced
Your Name <you@example.com>
2022-01-15 15:06:38 -0500

    page initally rendering

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './style.css'
+
+const App = () => {
+    return (
+        <div>Hey</div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
