55f912c367a676be30743bedd0bc65f29956a5fb
Your Name <you@example.com>
2022-01-17 15:27:56 -0500

    updated project listing

--- src/component/Projects/Projects.js
+++ src/component/Projects/Projects.js
@@ -9,8 +9,19 @@ const Projects = () => {
         <div className="projects">
             <h2 class = "portfolio-title">Just a few samples...</h2>
             <div class= "projectList">
+            <div class = "project">
+                    <h3 class = "projectTitle">Grace Shopper </h3>
+                    <div className='content'>
+                        <p class = "description">Frontend: E-commerce website, using React framework, where the user can have a cozy, friendly experience and register/login, view all products, add to cart and checkout (logged in or not). This project was built in a group of 4 fellow developers, remotely. Backend: E-commerce website with a functional database and API with role middleware authorization, user registration/login with password hashing and salting for elevated security, using Express, Morgan, jsonwebtoken, and more. Built with 4 co-developers. Frontend available.</p>
+                        <p>Technologies: PostgreSQL, Express, React, JavaScript, Node, Axios, CSS(viz. Material-UI)</p>
+                    </div>
+                    <a className='box' href="https://mclovins.netlify.app/" target="blank">
+                        <p className='info'>Click to check it out</p>
+                        <img src={pic3} alt = "picture of homepage for Grace Shopper" class="photo"/>
+                    </a>
+                </div>
                 <div class = "project">
-                    <h3 class = "projectTitle">Stranger’s Things </h3>
+                    <h3 class = "projectTitle">Strangers Things </h3>
                     <div className='content'>
                         <p class = "description">Message board facilitating consumer-to-consumer sales.Website connects with an API to allow for the creation, deletion, and editing of for-sale posts.Users can also message others and interact.To promote user activity, login & registration required for interactivity.React used to build out website and respond to data changes.</p>
                         <p>Technologies: React, JavaScript, Postman, Axios, CSS(viz. Styled Components)</p>
@@ -29,17 +40,6 @@ const Projects = () => {
                     <a className='box' href="https://fitness-tracker1.netlify.app" target="blank">
                         <p className='info'>Click to check it out</p>
                         <img src={pic2} alt = "picture of homepage for Fitness Tracker" class="photo" />
-                    </a>
-                </div>
-                <div class = "project">
-                    <h3 class = "projectTitle">Grace Shopper </h3>
-                    <div className='content'>
-                        <p class = "description">Frontend: E-commerce website, using React framework, where the user can have a cozy, friendly experience and register/login, view all products, add to cart and checkout (logged in or not). This project was built in a group of 4 fellow developers, remotely. Backend: E-commerce website with a functional database and API with role middleware authorization, user registration/login with password hashing and salting for elevated security, using Express, Morgan, jsonwebtoken, and more. Built with 4 co-developers. Frontend available.</p>
-                        <p>Technologies: PostgreSQL, Express, React, JavaScript, Node, Axios, CSS(viz. Material-UI)</p>
-                    </div>
-                    <a className='box' href="https://mclovins.netlify.app/" target="blank">
-                        <p className='info'>Click to check it out</p>
-                        <img src={pic3} alt = "picture of homepage for Grace Shopper" class="photo"/>
                     </a>
                 </div>
             </div>
