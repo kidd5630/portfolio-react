0142696f19047958de9047b12c5a99025bbf41cf
Your Name <you@example.com>
2022-05-04 23:46:30 -0400

    mobile friendly, ready to deploy

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -7,9 +7,9 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 const Footer = () => {
     return (
         <div className="footer">
-            <a href= "mailto:charles.d.suitt@gmail.com"><EmailIcon sx={{ fontSize: 80 }}/></a>
-            <a href="https://github.com/kidd5630" target="_blank"><GitHubIcon sx={{ 'color': 'black' },{ fontSize: 80 }} /></a>
-            <a href="https://www.linkedin.com/in/charles-suitt-a5a3ba4b/" target="_blank"><LinkedInIcon sx={{ color: 'pink[500]' },{ fontSize: 80 }}/></a>
+            <a href= "mailto:charles.d.suitt@gmail.com"><EmailIcon /></a>
+            <a href="https://github.com/kidd5630" target="_blank"><GitHubIcon /></a>
+            <a href="https://www.linkedin.com/in/charles-suitt-a5a3ba4b/" target="_blank"><LinkedInIcon /></a>
         </div>
     )
 }
